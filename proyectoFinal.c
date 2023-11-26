d61cbd3279ce0206775e4407d6036061282f8d8a
ElJavis43 <148113419+ElJavis43@users.noreply.github.com>
2023-11-26 02:38:09 -0600

    Update proyectoFinal.c

--- proyectoFinal.c
+++ proyectoFinal.c
@@ -4,6 +4,7 @@
 #include<time.h>
 #include<windows.h>
 #include<string.h>
+#include<stdbool.h>
 
 #define color SetConsoleTextAttribute
 
@@ -12,17 +13,21 @@ void generarClaves();
 void generarSalarios();
 void imprimirTabla();
 void imprimirRegistros();
-void buscarRegistro();//zahid
+int buscarRegistro(char[]);//zahid
 void ordenarRegistros();//zahid
 void insertarRegistros();//Abel
 void eliminarRegistros();//Abel
 void modificarRegisro();//Javi
+void barraBusqueda();
 void gotoxy(int x, int y);
 void menu();
+void tabla();
+void indicarRegistro();
 
+//https://www.youtube.com/watch?v=sEx8EdTO8R8&list=WL&index=10&t=259s&ab_channel=DuarteCorporationTutoriales
 //Vectores universales que guardan los nombres y los apellido que se van a utilizar
-char nombres[10][20] = {"Zahid", "Abel", "Javier", "Diego", "Alejandro", "Leonardo", "Daniel", "Maria", "Julieta", "Elisa"};
-char apellidos[10][20] = {"Rios", "Duron", "Garcia", "Tafoya", "Jimenez", "Hernandez", "Esparza", "Esquivel", "Guerrero", "Carrasco"};
+char nombres[10][20] = {"ZAHID", "ABEL", "JAVIER", "DIEGO", "ALEJANDRO", "LEONARDO", "DANIEL", "MARIA", "JULIETA", "ELISA"};
+char apellidos[10][20] = {"RIOS", "DURON", "GARCIA", "TAFOYA", "JIMENEZ", "HERNANDEZ", "ESPARZA", "ESQUIVEL", "GUERRERO", "CARRASCO"};
 //Matriz donde se van a guardar los nombres generados
 char nombresGenerados[10][50];
 //Vector donde se van a guardar las claves generadas
@@ -30,13 +35,13 @@ int clave[10];
 //Vector donde se van a guardar los salarios generados
 float salario[10];
 
-int i;
+int i, e;
 
 int main(){
 	setlocale(LC_ALL, "");
 	//La pantalla va a tener un estilo inicial de fondo blanco y texto negro
 	system("color f0");
-	//La pantalla va a tener 
+	//La pantalla va a tener 100 de ancho y 30 de largo
 	system("mode con: cols=100 lines=30");
 	srand(time(NULL));
 	generarSalarios();
@@ -56,11 +61,12 @@ void gotoxy(int x, int y){
 
 void menu(){
 	//menú principal
-		int o = 0;
-	int i = -1;
+	int o = 0;
+	int i = -1, e = 0;
 	
 	while(o != 7){
-			printf("\n\t\tBeinvenido al menu\n");
+	system("color f0");
+	printf("\n\t\tBeinvenido al menu\n");
 	printf("\n\t1 Mostrar todos los contactos:\n ");//Los tres registros con la info de el trabajador
 	printf("\n\t2 Buscar registro: \n");//opcion de nombre o clave
 	printf("\n\t3 Ordenar alfabeticamente por Nombre: \n");
@@ -70,6 +76,7 @@ void menu(){
 	printf("\n\t7 Salir\n");
 	
 	printf("\n\t Que opcion desea elegir: ");
+	fflush(stdin);
 	scanf("%d", &o);
 		if(o != 1, o!= 2, o != 3, o!= 4, o != 5, o != 6, o!= 7){
 			system("cls");
@@ -93,11 +100,41 @@ void menu(){
 		}
 		if(o == 2){
 			system("cls");
-			while(i !=0){
+			//Función que imprime la barra de busqueda
+			HANDLE consola = GetStdHandle(STD_OUTPUT_HANDLE);
+			char cadenaBuscar[50];
+			do{
+				imprimirTabla();
+				imprimirRegistros();
+				barraBusqueda();
+				imprimirTabla();
+				color(consola, 112);
+				//se coloca el cursor en la barra de busqueda
+				gotoxy(28, 1);
+				fflush(stdin);
+				gets(cadenaBuscar);
+				if(strcmp(cadenaBuscar, "0") == 0){
+					system("cls");
+					break;
+				}
+				imprimirTabla();
+				system("cls");
+				int existe = buscarRegistro(cadenaBuscar);
+				if(existe == 0){
+					printf("El nombre/clave %s no se encuentra en la base de datos intenta con otro", cadenaBuscar);
+				}else{
+					imprimirTabla();	
+				}
+				printf("\n\n\n\tPresiona cualquier tecla para regresar...");
+				system("pause>null");
+				system("cls");
+			}while(true);
+			/*while(i !=0){
 				printf("\n\t1 Buscar por nombre.\n");
 				printf("\n\t2 Buscar por clave.\n");
 				printf("\n\t0 Regresar al menu.\n");
 				printf("\n\t¿Como desea buscar el registro? ");
+				fflush(stdin);
 				scanf("%d", &i);
 				
 				if(i != 1 & i != 2, i!= 0){
@@ -117,7 +154,7 @@ void menu(){
 					menu();
 					//Regresar al menu
 				}
-			}
+			}*/
 		}	
 		if(o == 3){
 			system("cls");
@@ -133,35 +170,7 @@ void menu(){
 			}
 		if(o == 6){
 			system("cls");
-			while(i != 0){
-				printf("\n\t1 Modificar clave.");
-				printf("\n\t2 Modificar nombre.");
-				printf("\n\t3 Modificar el salario.");
-				printf("\n\t0 Regresar al menu principal");
-				printf("\n\t\t¿Que desea modoficar? ");
-				scanf("%d", &i);
-				
-				if(i != 1,2,3,0){
-					system("cls");
-					printf("\n\tIngrese una opcion valida\n");
-				}
-				if(i == 1){
-					system("cls");
-					//Modificar la clave
-				}
-				if(i == 2){
-					system("cls");
-					//Modificar clave
-				}
-				if(i == 3){
-					system("cls");
-					//Modificar salario
-				}
-				if(i == 0){//Regresar al menu
-					system("cls");
-					menu();
-				}
-			}
+			modificarRegistro();
 		}
 	}
 	printf("Menu");
@@ -246,9 +255,62 @@ void imprimirRegistros(){
 		printf("MXN");
 	}
 }
-//
+//Barra superior donde se va ingresar lo que se quiere buscar
+void barraBusqueda(){
+	HANDLE consola = GetStdHandle(STD_OUTPUT_HANDLE);
+	gotoxy(12, 0);
+	gotoxy(5, 1);
+	printf("Buscar(clave o nombre):");
+	color(consola, 112);
+	for(i = 0; i < 60; i++){
+		gotoxy(28 + i, 1);
+		printf(" ");
+	}
+	color(consola, 240);
+}
 
-void buscarRegistro(){
+int buscarRegistro(char cadena[]){
+	HANDLE consola = GetStdHandle(STD_OUTPUT_HANDLE);
+	strupr(cadena);
+	char nombresTemporales[10][50];
+	int clavesTemporales[10];
+	float salariosTemporales[10];
+	int tam = 0;
+	for(i = 0; i < 10; i++){
+		//https://parzibyte.me/blog/2019/12/21/convertir-cadena-entero-c-strtol/
+		if(strcmp(cadena, nombresGenerados[i]) == 0 || strtol(cadena, NULL, 10) == clave[i]){
+			strcpy(nombresTemporales[tam], nombresGenerados[i]);
+			clavesTemporales[tam] = clave[i];
+			salariosTemporales[tam] = salario[i];
+			gotoxy(25 + i, 25);
+			tam++;
+		}
+	}
+	if(tam == 0){
+		return tam;
+	}
+	
+	for(i = 0; i < tam; i++){
+		//Se imprimen las claves
+		gotoxy(15, i + 5);
+		color(consola, 248);
+		printf("%d", clavesTemporales[i]);
+		color(consola, 240);
+		gotoxy(27, i + 5);
+		//Linea para dividir las claves de los nombres
+		printf("|");
+		gotoxy(35, i + 5);
+		//Se imprimen los nombres
+		printf("%s", nombresTemporales[i]);
+		gotoxy(70, i + 5);
+		gotoxy(68, i + 5);
+		//Linea para dividir los nombres de los salarios
+		printf("|");
+		//Se imprimen los salarios
+		printf("$%.2f ", salariosTemporales[i]);
+		gotoxy(85, i + 5);
+		printf("MXN");
+	}
 	
 }
 //
@@ -265,6 +327,85 @@ void eliminarRegistros(){
 }
 //
 void modificarRegisro(){
-	
+	while(i != 0){
+			tabla();
+			printf("\n\t1 Modificar clave.");
+			printf("\n\t2 Modificar nombre.");
+			printf("\n\t3 Modificar el salario.");
+			printf("\n\t0 Regresar al menu principal");
+			printf("\n\t\t¿Que desea modoficar? ");
+			scanf("%d", &i);
+			e = i;
+			if(i != 1,2,3,0){
+				system("cls");
+				printf("\n\tIngrese una opcion valida\n");
+			}
+			if(i == 1){
+				int c, d, x;
+				//Modificar la clave
+				printf("\n\t\tIngrese la clave a modificar: ");
+				scanf("%d", &c);
+
+				printf("\t\tIngrese la nueva clave: ");
+				scanf("%d", &d);
+				//cliclo for que  va a remplazar la posison de la clave que se desea
+				for( x=0; x<10; x++){
+					//sustituir la clave original por la nueva
+					if(clave[x] == c){
+					clave[x] = d;
+					}
+				}									
+			}
+			//Para modificar el nombre, le probe de muchas maneras pero por alguna razon nome lee lo que ingreso
+			//y nunca me deja escribir el nombre nuevoq ue quiero ingresar
+			//los otros 2 ya funcionan, no se si quieras que les agregue algo mas
+			// para el 2 segui lit la misma logica pero por alguna rzon no funciona, me da error en los "="
+			/*if(i == 2){
+				char c, x, d; 
+				//Modificar nombre 
+				system("cls");
+				printf("\n\t\tIngrese el nombre a modificar: ");
+				scanf("%s", &c);
+					
+				printf("\t\tIngrese el nuevo nombre: ");
+				scanf("%s", &d);
+			
+				for( x=0; x<10; x++){
+					if( nombresGenerados[x] == c){
+						nombresGenerados[x] == d;
+					}
+				}
+			}*/
+			if(i == 3){
+				system("cls");
+				//Modificar salario
+				int c, d, x;
+				system("cls");
+				printf("\n\t\tIngrese el salario a modificar: ");
+				scanf("%d", &c);				
+				
+				printf("\t\tIngrese el nuevo salario: ");
+				scanf("%d", &d);
+				//cliclo for que  va a remplazar la posison de el slario que se desea
+				for( x=0; x<10; x++){
+					//sustituir el salario original por el nuevo
+					if(salario[x] == c){
+						salario[x] = d;
+					}
+				}
+			}
+			if(i == 0){//Regresar al menu
+				system("cls");
+				menu();
+			}
+		}
 }
 
+void tabla(){
+	gotoxy(40, 1);
+	
+	imprimirTabla();
+	imprimirRegistros();
+	printf("\n\n\t");
+	system("pause");
+}
